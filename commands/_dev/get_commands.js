c04dfa77c527846643768e263d7f2d4c00383370
Dan Howe <danhowe0@gmail.com>
2021-03-01 22:20:07 +0000

    g!commands testing

--- commands/_dev/get_commands.js
+++ commands/_dev/get_commands.js
@@ -1,9 +1,32 @@
+const Discord = require('discord.js'); 
+const fs = require("fs");
+
 module.exports = {
-  name: 'cmds',
-  category: 'dev',
-  aliases: [''],
-  description: '',
-  run: async (client, message, args) => {
-    console.log(client.commands.get())
-  }
-}
+    name: 'help',
+    description: 'Lists available commands',
+    cateegory: 'dev'
+
+    async run(client, message, args, con) {
+        fs.readdir("./commands/", (err, files) => {
+            if(err) console.error(err);
+    
+            let jsfiles = files.filter(f => f.split(".").pop() === "js");
+            if(jsfiles.length <= 0) {
+                console.log("No commands to load!");
+                return;
+            }
+            
+            var namelist = "";
+            var desclist = "";
+            
+            let result = jsfiles.forEach((f, i) => {
+                let props = require(`./${f}`);
+                namelist = props.name;
+                desclist = props.description;
+                message.author.send(`**${namelist}** \n${desclist} \n`);
+            });
+     
+
+     
+        });
+    }
