cef5b142f164dcf1903861be44a325de0be3ede4
umarf786 <onskadthecamel786@outlook.com>
2021-03-04 20:10:57 +0000

    Added channel send for log

--- commands/support/close.js
+++ commands/support/close.js
@@ -18,7 +18,8 @@ module.exports = {
           //
           if (err) throw err;
           if (data) {
-            client.channels.get('817124584729346049').send(ch.id);
+            const channel = member.guild.channels.find((ch) => ch.name === 'ticket-logs-discord');
+            channel.send(ch.id);
             fs.writeFileSync(`../${ch.id}.txt`, data.Content.join('\n\n'));
             transcriptChannel.send(`${message.guild.members.cache.get(ch.name).user.username}'s ticket have been closed.`);
             await transcriptChannel.send(new MessageAttachment(fs.createReadStream(`../${ch.id}.txt`)));
