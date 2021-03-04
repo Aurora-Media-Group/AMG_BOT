8c60a8d322bba8924d654e958a747e0bc580b02a
umarf786 <onskadthecamel786@outlook.com>
2021-03-04 20:04:32 +0000

    Sending logs to test channel

--- commands/support/close.js
+++ commands/support/close.js
@@ -1,30 +1,31 @@
-const { Message, Client, MessageAttachment} = require('discord.js')
-const fs = require('fs')
+const { Message, Client, MessageAttachment } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
-    name : 'close',
-    /**
-     * @param {Client} client
-     * @param {Message} message
-     */
-    run : async(client, message) => {
-        if(message.channel.parentID !== '816328167626768406') return message.channel.send('You can only use this command in a ticket!');
-        const transcriptChannel = message.guild.channels.cache.get('689584207114272768')
-        message.channel.send('Deleting ticket in 5 seconds.....')
-        setTimeout(() => {
-            message.channel.delete().then(async ch=> {
-              // This line has error which I guess is being thrown below
-                client.ticketTranscript.findOne({ Channel : ch.id }, async(err, data) => {
-              //
-                    if(err) throw err;
-                    if(data) {
-                        fs.writeFileSync(`../${ch.id}.txt`, data.Content.join("\n\n"))
-                        transcriptChannel.send(`${message.guild.members.cache.get(ch.name).user.username}'s ticket have been closed.`)
-                        await transcriptChannel.send(new MessageAttachment(fs.createReadStream(`../${ch.id}.txt`)));
-                        client.ticketTranscript.findOneAndDelete({ Channel : ch.id })
-                    }
-                })
-            })
-        }, 5000)
-    }
-}
+  name: 'close',
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   */
+  run: async (client, message) => {
+    if (message.channel.parentID !== '816328167626768406') return message.channel.send('You can only use this command in a ticket!');
+    const transcriptChannel = message.guild.channels.cache.get('689584207114272768');
+    message.channel.send('Deleting ticket in 5 seconds.....');
+    setTimeout(() => {
+      message.channel.delete().then(async (ch) => {
+        // This line has error which I guess is being thrown below
+        client.ticketTranscript.findOne({ Channel: ch.id }, async (err, data) => {
+          //
+          if (err) throw err;
+          if (data) {
+            client.channels.get('817124584729346049').send(ch.id);
+            fs.writeFileSync(`../${ch.id}.txt`, data.Content.join('\n\n'));
+            transcriptChannel.send(`${message.guild.members.cache.get(ch.name).user.username}'s ticket have been closed.`);
+            await transcriptChannel.send(new MessageAttachment(fs.createReadStream(`../${ch.id}.txt`)));
+            client.ticketTranscript.findOneAndDelete({ Channel: ch.id });
+          }
+        });
+      });
+    }, 5000);
+  },
+};
